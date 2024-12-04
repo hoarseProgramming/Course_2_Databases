03a86599f5a13654160df53e664a43c12242e858
hoarseProgramming <hampus.es@gmail.com>
2024-12-04 10:05:48 +0100

    Add L008 - SQL server database project

---
+++ Course_2_Databases/C#_Lectures/L008_SQL_Server_Database_Project/dbo/views/CityView.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[CityView] AS
+SELECT 
+	[ci].[Id], 
+	[ci].[Name] AS 'City', 
+	[ci].[CountryId],
+	[co].[Name] AS 'Country'
+FROM 
+	[dbo].[Cities] ci
+	left join [dbo].[Countries] co on ci.CountryId = co.Id
+
